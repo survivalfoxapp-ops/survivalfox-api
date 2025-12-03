86be39a96f889935cfe61376d63ea109c8544676
survivalfoxapp-ops <survivalfox.app@gmail.com>
2025-12-03 17:28:23 +0100

    Update main.py
    Add status endpoint

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hallo von der Survival Fox API in Docker!"}
+
+@app.get("/status")
+def read_status():
+    return {
+        "app": "Survival Fox API",
+        "status": "ok",
+        "version": "0.0.1"
+    }
